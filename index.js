fce37d6d797e17bb37f836c198daeeac0d2ae30a
thisiscpta <121990524+thisiscpta@users.noreply.github.com>
2023-01-26 16:09:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`WHERE ARE YOU Hello ${nameToGreet}!`);
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`THIS IS IT The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
